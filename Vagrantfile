77395a8cfe0f50ae7a33ee2275edb74337c1f369
Gustavo Valverde <g.valverde02@gmail.com>
2016-04-19 06:15:57 +0000

    Add Vagrant files to be used by users testing this tournament

---
+++ Vagrantfile
@@ -0,0 +1,41 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+Vagrant.configure(2) do |config|
+  config.vm.box = "ubuntu/wily64" # Ubuntu 15.10
+  config.vm.hostname = 'DevOps'
+  config.vm.network "forwarded_port", guest: 8000, host: 8000
+  config.vm.network "forwarded_port", guest: 8080, host: 8080
+  config.vm.network "forwarded_port", guest: 5000, host: 5000
+  # config.vm.synced_folder ".", "/vagrant", type: "rsync"
+
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.name = "DevOps"
+    vb.gui = "on"       # Display the VirtualBox GUI when booting the machine
+    vb.memory = 4096
+    vb.cpus = 2
+    vb.customize ["modifyvm", :id, "--vram", 128]
+    vb.customize ["modifyvm", :id, "--accelerate3d", "on"]
+    vb.customize ["modifyvm", :id, "--draganddrop", "bidirectional"]
+    vb.customize ["modifyvm", :id, "--clipboard", "bidirectional"]
+    vb.customize ["modifyvm", :id, '--audio', 'dsound', '--audiocontroller', 'ac97'] # choices: hda sb16 ac97
+    vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+    if RUBY_PLATFORM =~ /darwin/
+      vb.customize ["modifyvm", :id, '--audio', 'coreaudio', '--audiocontroller', 'hda'] # choices: hda sb16 ac97
+    elsif RUBY_PLATFORM =~ /mingw|mswin|bccwin|cygwin|emx/
+      vb.customize ["modifyvm", :id, '--audio', 'dsound', '--audiocontroller', 'ac97']
+    end
+  end
+
+  config.vm.provision "shell" do |s|
+    s.privileged = false
+    s.path = "scripts/devops.sh"
+  end
+end
---
+++ devops.sh
@@ -0,0 +1,116 @@
+echo "*********************************"
+echo "*                               *"
+echo "*      Adding repositories      *"
+echo "*                               *"
+echo "*********************************"
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+
+echo "*********************************"
+echo "*                               *"
+echo "*     Updating and upgrading    *"
+echo "*                               *"
+echo "*********************************"
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+echo "*********************************"
+echo "*                               *"
+echo "*        VBox Utilities         *"
+echo "*                               *"
+echo "*********************************"
+sudo apt-get install -y virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms
+
+echo "*********************************"
+echo "*                               *"
+echo "*        Installing Git         *"
+echo "*                               *"
+echo "*********************************"
+sudo apt-get install -y git
+git config --global color.ui true
+cd ~
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt
+echo 'GIT_PROMPT_ONLY_IN_REPO=1' >> ~/.bashrc
+echo 'GIT_PROMPT_SHOW_UNTRACKED_FILES=normal' >> ~/.bashrc
+echo 'GIT_PROMPT_THEME=Single_line_Solarized' >> ~/.bashrc
+echo 'source ~/.bash-git-prompt/gitprompt.sh' >> ~/.bashrc
+
+echo "*********************************"
+echo "*                               *"
+echo "*        My GitHub Reps         *"
+echo "*                               *"
+echo "*********************************"
+cd ~
+git clone https://github.com/gustavovalverde/swiss-tournament.git ~/swiss-tournaments
+
+echo "******************************************"
+echo "*                                        *"
+echo "*  Installing Python and dependencies    *"
+echo "*                                        *"
+echo "******************************************"
+sudo apt-get install -qq -y python-software-properties software-properties-common
+sudo apt-get install python
+sudo apt-get -qq -y install postgresql python-psycopg2
+sudo apt-get -qq -y install python-flask python-sqlalchemy
+sudo apt-get remove python-pip
+sudo -H  easy_install -U pip
+sudo -H  pip install bleach
+sudo -H  pip install oauth2client
+sudo -H  pip install requests
+sudo -H  pip install httplib2
+sudo -H  pip install redis
+sudo -H  pip install passlib
+sudo -H  pip install itsdangerous
+sudo -H  pip install flask-httpauth
+sudo -H  pip install flake8
+sudo su postgres -c 'createuser -dRS vagrant'
+sudo su vagrant -c 'createdb'
+sudo su vagrant -c 'createdb tournament'
+sudo su vagrant -c 'psql forum -i /vagrant/swiss-tournament/tournament.sql'
+
+
+echo "*********************************"
+echo "*                               *"
+echo "*     Installing Sublime 3      *"
+echo "*                               *"
+echo "*********************************"
+git clone https://github.com/gustavovalverde/sublime-text-seed.git ~/.sublime-seed
+cd ~/.sublime-seed
+./setup.py
+
+echo "*********************************"
+echo "*                               *"
+echo "*          redis-stable         *"
+echo "*                               *"
+echo "*********************************"
+cd ~
+sudo wget http://download.redis.io/redis-stable.tar.gz
+sudo tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+sudo make install
+
+echo "******************************************"
+echo "*                                        *"
+echo "*          Installing Browser            *"
+echo "*                                        *"
+echo "******************************************"
+sudo apt-get install -qq -y firefox
+
+echo "*********************************"
+echo "*                               *"
+echo "*   Installing MATE Desktop     *"
+echo "*                               *"
+echo "*********************************"
+sudo apt-get install -y ubuntu-mate-core ubuntu-mate-desktop
+
+echo "*********************************"
+echo "*                               *"
+echo "*        Time to reboot         *"
+echo "*                               *"
+echo "*********************************"
+  # have to reboot for drivers to kick in, but only the first time of course
+if [ ! -f ~/runonce ]
+then
+  sudo reboot
+  touch ~/runonce
+fi
